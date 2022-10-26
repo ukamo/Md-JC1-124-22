ce04946bea6936b0b46d33c2d4c1990f83b5a1e1
Julia Morozova <ukamo125@gmail.com>
2022-10-26 22:07:44 +0300

    change task3

--- Homework/src/home_work_1/Task3.java
+++ Homework/src/home_work_1/Task3.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        Scanner inputNumber1 = new Scanner(System.in);
+        Scanner inputNumber = new Scanner(System.in);
         System.out.print("Input a number 1: ");
-        int number1 = inputNumber1.nextInt();
+        int number1 = inputNumber.nextInt();
         System.out.print("Input a number 2: ");
-        int number2 = inputNumber1.nextInt();
+        int number2 = inputNumber.nextInt();
         int degreeNumbers = (int) Math.pow(number1,number2);
-        System.out.print("Degree of 2 Numbers are: " + degreeNumbers);
+        System.out.print("\n Degree of 2 Numbers are: " + degreeNumbers);
+        int ModuleNumber1 = (int) Math.abs(number1);
+        int ModuleNumber2 = (int) Math.abs(number2);
+        System.out.print("\n Module of the first Number is: " + ModuleNumber1);
+        System.out.print("\n Module of the second Number is: " + ModuleNumber2);
     }
 }
