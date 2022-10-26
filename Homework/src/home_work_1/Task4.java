1c96e3a1c7a867cff7aae4fc8c939d8b6e106b8f
Julia Morozova <ukamo125@gmail.com>
2022-10-26 14:56:11 +0300

    task 4 -add function about even and odd numbers

--- Homework/src/home_work_1/Task4.java
+++ Homework/src/home_work_1/Task4.java
@@ -1,4 +1,17 @@
 package home_work_1;
 
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner inputNumber1 = new Scanner(System.in);
+        System.out.print("Input number 1: ");
+        int number1 = inputNumber1.nextInt();
+
+        if ( number1 % 2 == 0 ) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
 }
