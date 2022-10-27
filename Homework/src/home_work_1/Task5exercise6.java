706e3365cb813b3a8dc5868232510a4e4d591008
Julia Morozova <ukamo125@gmail.com>
2022-10-27 22:03:42 +0300

    add task4 exercise 6 additional

--- Homework/src/home_work_1/Task5exercise6.java
+++ Homework/src/home_work_1/Task5exercise6.java
@@ -8,16 +8,29 @@ public class Task5exercise6 {
         System.out.print ("Введите год: ");
         Scanner input = new Scanner(System.in);
         int yearFromUser = input.nextInt();
-        if (yearFromUser % 4 != 0) {
+
+        if (yearFromUser % 4 != 0 || yearFromUser % 100 == 0 && yearFromUser % 400 != 0) {
             System.out.println("Год обычный");
-        } else if (yearFromUser % 100 == 0){
-                if (yearFromUser % 400 == 0) {
-                    System.out.println("Год високосный");
-                } else {
-                    System.out.println("Год обычный");
-                }
         } else {
             System.out.println("Год високосный");
         }
+      /*  if (yearFromUser % 4 == 0 && yearFromUser % 100 != 0 || yearFromUser % 400 == 0 &&
+        yearFromUser % 100 == 0 && yearFromUser % 400 == 0 ) {
+            System.out.println("Год високосный");
+        } else {
+            System.out.println("Год обычный");
+        } */
+
+//        if (yearFromUser % 4 != 0) {
+//            System.out.println("Год обычный");
+//        } else if (yearFromUser % 100 == 0){
+//                if (yearFromUser % 400 == 0) {
+//                    System.out.println("Год високосный");
+//                } else {
+//                    System.out.println("Год обычный");
+//                }
+//        } else {
+//            System.out.println("Год високосный");
+//        }
     }
 }
