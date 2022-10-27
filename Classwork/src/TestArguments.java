35c79e1d263731adfe9c1b24cda6408f6f891602
Julia Morozova <ukamo125@gmail.com>
2022-10-27 22:44:18 +0300

    change folders

--- Classwork/src/TestArguments.java
+++
@@ -1,9 +0,0 @@
-public class TestArguments {
-
-    public static void main(String[] args) {
-        System.out.println("Mass +" + args.length);
-        for (String arg : args){
-            System.out.println(arg);
-        }
-    }
-}
