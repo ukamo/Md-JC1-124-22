98989a364324d30fb95bc4c3e6adf9b79869309e
Julia Morozova <ukamo125@gmail.com>
2022-10-28 23:56:38 +0300

    add Task5

---
+++ src/home_work_1/Task5.java
@@ -0,0 +1,34 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+
+
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.print ("Ввод дня недели ");
+        Scanner input = new Scanner(System.in);
+        int numberOfWeek = input.nextInt();
+        System.out.print ("У вас отпуск? true или false ");
+        boolean dayOfWeekends = input.nextBoolean();
+        boolean weekday = false;
+        if (numberOfWeek >=6 && numberOfWeek<=7){
+            weekday = true;
+        }
+       boolean resultOfFunction = sleepIn(weekday, dayOfWeekends);
+        if (resultOfFunction){
+            System.out.print ("Спим дальше");
+        } else{
+            System.out.print ("Пора идти на работу");
+        }
+
+    }
+
+    public static boolean sleepIn (boolean weekday, boolean dayOfWeekends){
+        if (weekday || dayOfWeekends){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
