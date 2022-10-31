c6e501dfa11bcaff6fd0e7c0aa491d4e6d7d910b
Julia Morozova <ukamo125@gmail.com>
2022-10-31 23:57:03 +0300

    add task2_1

--- src/home_work_1/Task5.java
+++ src/home_work_1/Task5.java
@@ -30,5 +30,7 @@ public class Task5 {
         } else {
             return false;
         }
+
+        // return weekday || dayOfWeekends;
     }
 }
---
+++ src/home_work_2/loops/Task2_1.java
@@ -0,0 +1,30 @@
+package home_work_2.loops;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task2_1 {
+    public static void main(String[] args) {
+        System.out.print("Введите целое число от 1 до 63 для определения факториала: ");
+        Scanner input = new Scanner(System.in);
+
+        if(input.hasNextLong()) {
+            long numberFromUser1 = Long.parseLong(input.next());
+            int minRez = 1;
+            int maxRez = 63;
+            if (numberFromUser1 >= minRez && numberFromUser1 <= maxRez ){
+                long resultNumb = 1 ;
+                for (long i = 1; i <= numberFromUser1; i++){
+                    //resultNumb = resultNumb * i;
+                    long y = resultNumb;
+                    resultNumb = resultNumb * i;
+                }
+                System.out.println(resultNumb);
+            } else {
+                System.out.println("Вы ввели неверное значение");
+            }
+        } else {
+            System.out.println("Вы ввели не целое число");
+        }
+    }
+}
